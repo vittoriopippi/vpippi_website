a58154dfcc12095d992ae48719594cc6298353c7
Vittorio Pippi <219424@studenti.unimore.it>
2024-01-13 17:44:57 +0100

    Update ALLOWED_HOSTS in settings.py

--- vpippi/vpippi/settings.py
+++ vpippi/vpippi/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zn(1)#_=xxs3$kicxk%2f^r5n_8@)(*3&4r*wxgav%7&az@@r%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'www.vpippi.com',
+]
 
 
 # Application definition
