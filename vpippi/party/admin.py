7e8478b3a948f088690eca5688ccb79ef3960ee8
Vittorio Pippi <219424@studenti.unimore.it>
2024-02-22 00:06:25 +0100

    Update admin display and add fuorisede card in invite_oktober.html

--- vpippi/party/admin.py
+++ vpippi/party/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Invite
 
 class InviteAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone', 'state', 'date_created', 'date_modified')
+    list_display = ('name', 'phone', 'state', 'date_modified')
     search_fields = ('name', 'phone', 'state')
     list_filter = ('state',)
 
