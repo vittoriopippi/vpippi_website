2bb012a2b5521fdfeaa1e2a4d493b7ecbc0c9dc6
Vittorio Pippi <219424@studenti.unimore.it>
2024-02-21 23:44:09 +0100

    Add admin customization for Invite model

--- vpippi/party/admin.py
+++ vpippi/party/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Invite
 
-admin.site.register(Invite)
+class InviteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'state', 'date_created', 'date_modified')
+    search_fields = ('name', 'phone', 'state')
+    list_filter = ('state',)
+
+admin.site.register(Invite, InviteAdmin)
