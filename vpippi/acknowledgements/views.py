7a587be96a94f599f27c12cde621b2852810e734
Vittorio Pippi <219424@studenti.unimore.it>
2025-06-20 17:32:18 +0200

    Add countdown feature to acknowledgements page with styling and script

--- vpippi/acknowledgements/views.py
+++ vpippi/acknowledgements/views.py
@@ -1,33 +1,63 @@
+from datetime import datetime
+
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from django.views import View
+
 from .models import Acknowledgement, LinkAcknowledgement, FakeAcknowledgement
+
 import re
 from random import choice
 
+
 class AcknowledgementView(View):
+    # helper to check the countdown condition
+    def _should_countdown(self, request):
+        # make the cutoff timezone-aware in the current TZ
+        cutoff = timezone.make_aware(
+            datetime(2025, 6, 20, 18, 15),
+            timezone.get_current_timezone()
+        )
+        return (not request.user.is_staff) and (timezone.now() < cutoff)
+
+    def dispatch(self, request, *args, **kwargs):
+        # if we’re before the cutoff and the user isn’t staff, show the countdown
+        if self._should_countdown(request):
+            return render(request, 'acknowledgements/countdown.html')
+        # otherwise proceed to get/post as normal
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
         return render(request, 'acknowledgements/index.html')
 
     def post(self, request):
         name_surname = request.POST.get('name_surname')
-        if name_surname is None:
+        if not name_surname:
             return render(request, 'acknowledgements/index.html')
 
-        name_surname = name_surname.lower()
-        name_surname = re.sub('[^a-z ]', '', name_surname)
+        # normalize input
+        cleaned = re.sub('[^a-z ]', '',
+                         name_surname.lower())
 
         try:
-            acknowledgement = Acknowledgement.objects.get(name_surname=name_surname)
-            name = acknowledgement.name
-            text = acknowledgement.text
+            ack = Acknowledgement.objects.get(
+                name_surname=cleaned
+            )
+            name, text = ack.name, ack.text
+
         except Acknowledgement.DoesNotExist:
             try:
-                link_acknowledgement = LinkAcknowledgement.objects.get(alt=name_surname)
-                name = link_acknowledgement.ack.name
-                text = link_acknowledgement.ack.text
-            except LinkAcknowledgement.DoesNotExist:
-                fake_acknowledgement = choice(FakeAcknowledgement.objects.all())
-                name = name_surname.title()
-                text = fake_acknowledgement.text.replace('<name>', name)
+                link_ack = LinkAcknowledgement.objects.get(
+                    alt=cleaned
+                )
+                name, text = link_ack.ack.name, link_ack.ack.text
 
-        return render(request, 'acknowledgements/acknowledgement.html', {'name': name, 'text': text})
+            except LinkAcknowledgement.DoesNotExist:
+                fake = choice(FakeAcknowledgement.objects.all())
+                name = name_surname.title()
+                text = fake.text.replace('<name>', name)
+
+        return render(request, 'acknowledgements/acknowledgement.html', {
+            'name': name,
+            'text': text
+        })
--- vpippi/main/views.py
+++ vpippi/main/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Paper
 
 def index(request):
-    papers = Paper.objects.select_related('conference').prefetch_related('authors', 'tags').order_by('-publication_date')
+    # if the user is not logged in or not staff, redirect:
+    if not request.user.is_staff:
+        return redirect('https://scholar.google.com/citations?user=OHmt2vUAAAAJ')
+
+    # otherwise load and render the index as before
+    papers = (
+        Paper.objects
+        .select_related('conference')
+        .prefetch_related('authors', 'tags')
+        .order_by('-publication_date')
+    )
     return render(request, 'main/index.html', {'papers': papers})
